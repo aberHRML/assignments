d1ca9a2d63b471eeadee3bc1e5d2a08770ee432b
jasenfinch <jsf9@aber.ac.uk>
2017-08-17 14:53:00 +0100

    removed tests for removed functions

--- tests/testthat/test-calcM.R
+++
@@ -1,7 +0,0 @@
-
-context('calcM')
-
-test_that('calcM returns correctly',{
-  res <- MFassign:::calcM(134.0176,'C13','[M-H]1-')
-  expect_true(class(res) == 'numeric')
-})
--- tests/testthat/test-calcMZ.R
+++
@@ -1,7 +0,0 @@
-
-context('calcMZ')
-
-test_that('calcMZ returns correctly',{
-  res <- MFassign:::calcM(134.0215,'C13','[M-H]1-')
-  expect_true(class(res) == 'numeric')
-})
