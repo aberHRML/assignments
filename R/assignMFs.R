d537a0db5377887af014c876beb6ff74a0d08335
jasenfinch <jsf9@aber.ac.uk>
2017-11-13 14:31:15 +0000

    retention time and mz added to correlations by assignMFs

--- R/assignMFs.R
+++ R/assignMFs.R
@@ -3,6 +3,7 @@
 #' @param correlations table containing correlations of m/z to assign molecular formulas
 #' @param parameters an S4 object of class AssignmentParamters containing the parameters for molecular formula assignment
 #' @importFrom tibble tibble
+#' @importFrom stringr str_split_fixed
 #' @examples 
 #' \dontrun{
 #' res <- assignMFs(correlations,assignmentParameters('FIE'))
@@ -11,14 +12,52 @@
 
 assignMFs <- function(correlations,parameters) {
   options(digits = 10)
+  
+  correlations <- correlations %>%
+    mutate(Mode1 = str_split_fixed(Feature1,'@',2) %>% 
+             as_tibble() %>% 
+             select(V1) %>% 
+             unlist() %>% 
+             str_sub(1,1),
+           Mode2 = str_split_fixed(Feature2,'@',2) %>% 
+             as_tibble() %>% 
+             select(V1) %>% 
+             unlist() %>% 
+             str_sub(1,1),
+           `m/z1` = str_split_fixed(Feature1,'@',2) %>% 
+             as_tibble() %>% 
+             select(V1) %>% 
+             unlist() %>% 
+             str_replace_all('[:alpha:]','') %>% 
+             as.numeric(),
+           `m/z2` = str_split_fixed(Feature2,'@',2) %>% 
+             as_tibble() %>% 
+             select(V1) %>% 
+             unlist() %>% 
+             str_replace_all('[:alpha:]','') %>% 
+             as.numeric(),
+           rt1 = str_split_fixed(Feature1,'@',2) %>% 
+             as_tibble() %>% 
+             select(V2) %>%
+             unlist() %>%
+             as.numeric(),
+           rt2 = str_split_fixed(Feature1,'@',2) %>% 
+             as_tibble() %>% 
+             select(V2) %>%
+             unlist() %>%
+             as.numeric()
+    ) %>%
+    select(Feature1,Feature2,Mode1:rt2,log2IntensityRatio,r)
+  
   assignment <- new('Assignment',
-      parameters = parameters,
-      correlations = correlations,
-      relationships = tibble(),
-      addIsoAssign = list(),
-      transAssign = list(),
-      assignments  = tibble()
-      )
+                    parameters = parameters,
+                    correlations = correlations,
+                    relationships = tibble(),
+                    addIsoAssign = list(),
+                    transAssign = list(),
+                    assignments  = tibble()
+  )
+  
   method <- assignMethods(assignment@parameters@technique)
   assignment <- method(assignment)
   return(assignment)
--- R/relationships-method.R
+++ R/relationships-method.R
@@ -13,27 +13,18 @@ setMethod('relationships',signature = 'Assignment',
             adducts <- lapply(parameters@adducts,function(y){tibble(Adduct = y)})
             adducts <- bind_rows(adducts,.id = 'Mode')
             
-            cors <- x@correlations %>%
-              mutate(Mode1 = str_sub(Feature1,1,1),
-                     Mode2 = str_sub(Feature2,1,1),
-                     Feature1 = as.numeric(str_replace_all(Feature1,'[:alpha:]','')), 
-                     Feature2 = as.numeric(str_replace_all(Feature2,'[:alpha:]',''))
-              )
-            clus <- makeCluster(parameters@nCores,type = parameters@clusterType)
-            rel <- parApply(clus,select(cors,Feature1,Feature2,Mode1,Mode2),1,function(y,limit,add,iso,trans){
-              mzAnnotation::relationshipPredictor(as.numeric(y[1:2]),limit = limit,modes = y[3:4],adducts = add,isotopes = iso,transformations = trans)
-            },limit = parameters@limit, add = parameters@adducts, iso = parameters@isotopes,trans = parameters@transformations)
-            stopCluster(clus)
+            cors <- x@correlations
             
-            rel <- suppressWarnings(bind_rows(rel)) %>% 
-              tbl_df() %>% 
-              mutate(Error = as.numeric(Error)) %>%
-              filter(Adduct1 %in% adducts$Adduct, Adduct2 %in% adducts$Adduct) %>%
-              inner_join(cors,by = c('m/z1' = 'Feature1','m/z2' = 'Feature2')) %>%
-              semi_join(adducts,by = c('Mode1' = 'Mode', 'Adduct1' = 'Adduct')) %>%
-              semi_join(adducts,by = c('Mode2' = 'Mode', 'Adduct2' = 'Adduct')) %>% 
-              filter(Isotope1 %in% c(NA,parameters@isotopes) & Isotope2 %in% c(NA,parameters@isotopes)) %>%
-              select(`m/z1`:r)
+            clus <- makeCluster(parameters@nCores,type = parameters@clusterType)
+            rel <- parApply(clus,select(cors,`m/z1`,`m/z2`,Mode1,Mode2),1,function(y,limit,add,iso,trans){
+              mzAnnotation::relationshipPredictor(as.numeric(y[1:2]),limit = limit,modes = y[3:4],adducts = add,isotopes = iso,transformations = trans)
+            },limit = parameters@limit, add = parameters@adducts, iso = parameters@isotopes,trans = parameters@transformations) %>%
+              bind_rows() %>%
+              as_tibble() %>%
+              inner_join(cors,by = c('m/z1' = 'm/z1','m/z2' = 'm/z2')) %>%
+              select(Feature1:Mode2,`m/z1`,`m/z2`,rt1,rt2,Adduct1:Transformation2,log2IntensityRatio,r,Error)
+              
+            stopCluster(clus)
             
             x@relationships <- rel
             
