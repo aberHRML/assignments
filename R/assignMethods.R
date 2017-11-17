1dea9383e9a43d7a0cc50c59751609380ca97ed2
jasenfinch <jsf9@aber.ac.uk>
2017-11-17 12:53:42 +0000

    moved LCassignment to it's own file and added NP-LC to methods

--- R/assignMethods.R
+++ R/assignMethods.R
@@ -15,29 +15,10 @@ assignMethods <- function(method = NULL) {
           break()
         }
       }
-    `RP-LC` = function(assignment){
-      assignment@correlations <- assignment@correlations %>%
-        mutate( rt1 = str_split_fixed(Feature1,'@',2) %>% 
-                  as_tibble() %>% 
-                  select(V2) %>%
-                  unlist() %>%
-                  as.numeric(),
-                rt2 = str_split_fixed(Feature2,'@',2) %>% 
-                  as_tibble() %>% 
-                  select(V2) %>%
-                  unlist() %>%
-                  as.numeric(),
-                rtDiff = abs(rt1 - rt2)
-        ) %>%
-        filter(rtDiff <= parameters@RTwindow) %>%
-        select(Feature1:r)
-      assignment <- assignment %>%
-        prepCorrelations() %>%
-        relationships() %>%
-        addIsoAssign()
-    }
       return(assignment)
-    }
+    },
+    `RP-LC` = LCassignment,
+    `NP-LC` = LCassignment
   )
   
   if (is.null(method)) {
