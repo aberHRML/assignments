389743f6e95bea221dd6013b2df4ee3749b026b0
jasenfinch <jsf9@aber.ac.uk>
2018-06-29 16:04:11 +0100

    changed LC assignment method to accomodate failsafe

--- R/LCassignment.R
+++ R/LCassignment.R
@@ -1,25 +1,38 @@
 
-LCassignment <- function(assignment){
-  parameters <- assignment@parameters
-    assignment@correlations <- assignment@correlations %>%
-      mutate( rt1 = str_split_fixed(Feature1,'@',2) %>% 
-                as_tibble() %>% 
-                select(V2) %>%
-                unlist() %>%
-                as.numeric(),
-              rt2 = str_split_fixed(Feature2,'@',2) %>% 
-                as_tibble() %>% 
-                select(V2) %>%
-                unlist() %>%
-                as.numeric(),
-              rtDiff = abs(rt1 - rt2)
-      ) %>%
-      filter(rtDiff <= parameters@RTwindow) %>%
-      select(Feature1:r)
-    assignment <- assignment %>%
-      prepCorrelations() %>%
-      relationships() %>%
-      addIsoAssign()
-    
-  return(assignment)
+LCassignment <- function(element){
+  methods <- list(
+    prepCorrelations = function(assignment){
+      parameters <- assignment@parameters
+      assignment@correlations <- assignment@correlations %>%
+        mutate( rt1 = str_split_fixed(Feature1,'@',2) %>% 
+                  as_tibble() %>% 
+                  select(V2) %>%
+                  unlist() %>%
+                  as.numeric(),
+                rt2 = str_split_fixed(Feature2,'@',2) %>% 
+                  as_tibble() %>% 
+                  select(V2) %>%
+                  unlist() %>%
+                  as.numeric(),
+                rtDiff = abs(rt1 - rt2)
+        ) %>%
+        filter(rtDiff <= parameters@RTwindow) %>%
+        select(Feature1:r)
+      assignment %>%
+        prepCorrelations()
+    },
+    relationships = function(assignment){
+      assignment %>% 
+        relationships()
+    },
+    adductIsotopeAssignment = function(assignment){
+      assignment %>%
+        addIsoAssign()
+    }
+  )
+  
+  if (!is.null(elements)) {
+    method <- methods[[element]]
+  } 
+  return(method)
 }
