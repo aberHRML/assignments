fd464aefca043443cc8606f957524daa98f41bc8
jasenfinch <jsf9@aber.ac.uk>
2019-07-17 09:44:06 +0000

    added example peak data

---
+++ R/peakData.R
@@ -0,0 +1,5 @@
+#' peakData
+#' @description example peak intensity table of sample data from the example FIE-HRMS B. distachyon ecotype data in the metaboData package.
+#' @format A tibble containining 60 rows and 1003 variables 
+
+'peakData'
