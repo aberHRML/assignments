1747ba1bbf38b5fc73f52659fd0106ee21337bfd
jasenfinch <jsf9@aber.ac.uk>
2021-04-29 12:22:45 +0100

    relationships method now uses future_map for parallelisation

--- R/relationships-method.R
+++ R/relationships-method.R
@@ -1,6 +1,6 @@
-#' @importFrom parallel makeCluster parApply stopCluster
+#' @importFrom furrr future_map
 #' @importFrom dplyr mutate bind_rows tbl_df filter
-#' @importFrom dplyr inner_join semi_join select
+#' @importFrom dplyr inner_join semi_join select mutate_at
 #' @importFrom stringr str_sub str_replace_all
 #' @importFrom mzAnnotation relationshipCalculator
 #' @importFrom magrittr %>%
@@ -25,39 +25,37 @@ setMethod('relationships',signature = 'Assignment',
               trans <- c()
             }
             
-            clus <- makeCluster(parameters@nCores,type = parameters@clusterType)
             rel <- cors %>%
               select(`m/z1`,`m/z2`,Mode1,Mode2) %>%
               split(1:nrow(.)) %>%
-              parLapply(cl = clus,function(y,limit,add,iso,trans,addRules,isoRules,transRules){
-              mzAnnotation::relationshipCalculator(y %>%
-                                                     select(`m/z1`,`m/z2`) %>%
-                                                     unlist(),
-                                                   limit = limit,
-                                                   modes = y %>%
-                                                     select(Mode1,Mode2) %>%
-                                                     unlist(),
-                                                   adducts = add,
-                                                   isotopes = iso,
-                                                   transformations = trans,
-                                                   adductTable = addRules,
-                                                   isotopeTable = isoRules,
-                                                   transformationTable = transRules)
-            },
-            limit = parameters@limit,
-            add = parameters@adducts, 
-            iso = parameters@isotopes,
-            trans = trans,
-            addRules = parameters@adductRules,
-            isoRules = parameters@isotopeRules,
-            transRules = parameters@transformationRules
-            ) %>%
+              future_map(~{
+                relationshipCalculator(.x %>% 
+                                         select(`m/z1`,`m/z2`) %>% 
+                                         unlist(),
+                                       limit = parameters@limit,
+                                       modes = .x %>% 
+                                         select(Mode1,Mode2) %>% 
+                                         unlist(),
+                                       adducts = parameters@adducts, 
+                                       isotopes = parameters@isotopes,
+                                       transformations = trans,
+                                       adductTable = parameters@adductRules,
+                                       isotopeTable = parameters@isotopeRules,
+                                       transformationTable = parameters@transformationRules)
+              }) %>%
               bind_rows() %>%
               inner_join(cors,by = c('m/z1' = 'm/z1','m/z2' = 'm/z2')) %>%
-              select(Feature1:Mode2,`m/z1`,`m/z2`,RetentionTime1,RetentionTime2,Adduct1:Transformation2,log2IntensityRatio,r,Error,ID) %>%
-              mutate(RetentionTime1 = as.numeric(RetentionTime1),RetentionTime2 = as.numeric(RetentionTime2))
-            
-            stopCluster(clus)
+              select(Feature1:Mode2,
+                     `m/z1`,
+                     `m/z2`,
+                     RetentionTime1,
+                     RetentionTime2,
+                     Adduct1:Transformation2,
+                     log2IntensityRatio,
+                     r,
+                     Error,
+                     ID) %>%
+              mutate_at(vars(RetentionTime1,RetentionTime2),as.numeric)
             
             assignment@relationships <- rel
             
