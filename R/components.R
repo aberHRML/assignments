89badb289331cddfb2f9be8bfc0fb1074557a6e4
jasenfinch <jsf9@aber.ac.uk>
2021-11-19 09:34:38 +0000

    move component calculation functions to the same file

--- R/components.R
+++ R/components.R
@@ -34,11 +34,51 @@ calcComponents <- function(MFs,rel,parameters) {
     morph(to_components) %>%
     mutate(Size = graph_size(),
            Nodes = n(),
+           Degree = degree(Nodes,Size),,
+           Density = (2 * Size) / (Nodes * (Nodes - 1)),
+           Weight = sum(Weight) / Nodes,
+           AIS = sum(AddIsoScore) / Nodes,
+           Plausibility = plausibility(AIS,Degree,Weight)) %>%
+    unmorph()
+  
+  return(graph)
+}
+
+#' @importFrom tidygraph to_components
+#' @importFrom dplyr n
+
+recalcComponents <- function(graph,parameters){
+  g <- graph %>%
+    activate(nodes)
+  
+  comp <- g %>%
+    nodes() %>%
+    .$Component %>%
+    unique()
+  
+  weights <- comp %>%
+    future_map(~{
+      graph %>%
+        filter(Component == .x) %>%
+        edges() %>% 
+        .$r %>% 
+        mean() %>%
+        tibble(Weight = .)
+    },graph = g) %>%
+    set_names(comp) %>%
+    bind_rows(.id = 'Component') %>%
+    mutate(Component = as.numeric(Component))
+  
+  g %>%
+    select(-Weight) %>%
+    left_join(weights,by = 'Component') %>%
+    morph(to_components) %>%
+    mutate(Size = graph_size(),
+           Nodes = n(),
+           Degree = degree(Nodes,Size),
            Density = (2 * Size) / (Nodes * (Nodes - 1)),
            Weight = sum(Weight) / Nodes,
            AIS = sum(AddIsoScore) / Nodes,
            Plausibility = plausibility(AIS,Size,Weight)) %>%
     unmorph()
-  
-  return(graph)
 }
