e2ddd2b2c6b9911360a7e78e4eec913f666a43d0
jasenfinch <jsf9@aber.ac.uk>
2021-11-19 09:34:49 +0000

    added degree function

--- R/internals.R
+++ R/internals.R
@@ -1,4 +1,8 @@
 
-plausibility <- function(AIS,Size,Weight){
-  AIS + Weight + Size/10
+degree <- function(n_nodes,n_edges){
+  2 * (n_edges / n_nodes)
+}
+
+plausibility <- function(AIS,degree,weight){
+  AIS + weight + degree/10
 }
