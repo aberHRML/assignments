a87d9243469798df94f82a15302513ec985b5c19
jasenfinch <jsf9@aber.ac.uk>
2021-11-19 17:57:52 +0000

    moved elemental maximum frequency calculation to function outside of MFgen()

--- R/MFgen.R
+++ R/MFgen.R
@@ -1,7 +1,4 @@
-#' @importFrom mzAnnotation generateMF
-#' @importFrom tibble as_tibble
-
-MFgen <- function(M,mz,ppm = 6){
+maxElementFrequencies <- function(M){
   carb <- round(M/12)
   Hs <- round(carb * 2)
   NO <- round(carb / 2)
@@ -14,6 +11,17 @@ MFgen <- function(M,mz,ppm = 6){
             P = PS,
             S = PS)
   
+  return(maxi)
+}
+
+
+#' @importFrom mzAnnotation generateMF
+#' @importFrom tibble as_tibble
+
+MFgen <- function(M,mz,ppm = 6){
+  
+  max_element_frequencies <- maxElementFrequencies(M)
+  
   if (M < 100) {
     ppm <- 10
   } else {
@@ -23,12 +31,15 @@ MFgen <- function(M,mz,ppm = 6){
   ppm <- (ppm/10^6 * mz)/M * 10^6
   
   if (M < 200) {
-    gr <- F
+    gr <- FALSE
   } else {
-    gr <- T
+    gr <- TRUE
   }
   
-  res <- generateMF(M,ppm = ppm,charge = 0,validation = gr,element_max = maxi) %>% 
+  res <- generateMF(M,ppm = ppm,
+                    charge = 0,
+                    validation = gr,
+                    element_max = max_element_frequencies) %>% 
     rename(`Theoretical M` = Mass) %>%
     mutate(`Measured M` = M, `Measured m/z` = mz)
   return(res)
