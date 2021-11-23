4554266dd8776225ce9cd42053fca5507786f737
jasenfinch <jsf9@aber.ac.uk>
2021-11-23 12:56:42 +0000

    moved assignmentParameters to parameters file

--- R/parameters.R
+++ R/parameters.R
@@ -361,3 +361,36 @@ setMethod('transformationRules<-',signature = 'AssignmentParameters',
             x@transformationRules <- value
             return(x)
           })
+
+#' Assignment parameters
+#' @description Return default assignment parameters for a given technique.
+#' @param technique technique to use for assignment. \code{NULL} prints available techniques
+#' @importFrom parallel detectCores
+#' @importFrom methods new
+#' @export
+
+assignmentParameters <- function(technique = NULL){
+  availTechniques <- c('FIE','RP-LC','NP-LC')
+  if (is.null(technique)) {
+    cat('\nAvailable Techniques:',str_c('\n\t\t\t',str_c(availTechniques,collapse = '\n\t\t\t'),'\n'))
+    p <- NULL
+  } else {
+    
+    if (technique == 'FIE') {
+      p <- new('AssignmentParameters')
+    }
+    if (technique == 'RP-LC') {
+      p <- new('AssignmentParameters',
+               technique = 'RP-LC',
+               RTwindow = 1/60
+      )
+    }
+    if (technique == 'NP-LC') {
+      p <- new('AssignmentParameters',
+               technique = 'NP-LC',
+               RTwindow = 1/60
+      )
+    }
+  }
+  return(p)
+} 
