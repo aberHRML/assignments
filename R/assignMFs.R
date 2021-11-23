3095ed911adde50664a9a18d98bdd80328ee7b42
jasenfinch <jsf9@aber.ac.uk>
2021-11-23 13:44:34 +0000

    moved assignMethods() to assignMFs file

--- R/assignMFs.R
+++ R/assignMFs.R
@@ -1,4 +1,19 @@
 
+assignMethods <- function(method = NULL) {
+  methods <- list(
+    FIE = FIEassignment,
+    `RP-LC` = LCassignment,
+    `NP-LC` = LCassignment
+  )
+  
+  if (is.null(method)) {
+    method <- methods
+  } else {
+    method <- methods[[method]]
+  }
+  return(method)
+}
+
 setGeneric('doAssignment',function(assignment){
   standardGeneric('doAssignment')
 })
