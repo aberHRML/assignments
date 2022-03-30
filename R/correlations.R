88259ea5f5f83df438d10bdf5da317964f4a16a7
jasenfinch <jsf9@aber.ac.uk>
2022-03-30 21:29:17 +0000

    simplify correlations

--- R/correlations.R
+++ R/correlations.R
@@ -2,9 +2,7 @@
 setGeneric('calcCorrelations', function(assignment)
   standardGeneric('calcCorrelations'))
 
-#' @importFrom metabolyseR analysisParameters metabolyse analysisResults keepFeatures analysisData dat sinfo
-#' @importFrom magrittr set_rownames
-#' @importFrom stats cutree dist hclust
+#' @importFrom metabolyseR analysisParameters metabolyse analysisResults
 
 setMethod('calcCorrelations',signature = 'Assignment',function(assignment){
   if (assignment@log$verbose == T) {
@@ -17,47 +15,11 @@ setMethod('calcCorrelations',signature = 'Assignment',function(assignment){
   
   p@correlations <- parameters@correlations_parameters
   
-  if (str_detect(parameters@technique,'LC')) {
-    feat <- tibble(Feature = colnames(assignment@data)) %>%
-      mutate(RT = str_split_fixed(Feature,'@',2)[,2] %>%
-               as.numeric())
-    RTgroups <- feat %>%
-      data.frame() %>%
-      set_rownames(.$Feature) %>%
-      select(-Feature) %>%
-      dist() %>%
-      hclust() %>%
-      cutree(h = parameters@RT_window) %>%
-      {tibble(Feature = names(.),Group = .)}
-    
-    RTsum <- RTgroups %>%
-      group_by(Group) %>%
-      summarise(N = n())
-    
-    RTgroups <- RTgroups %>%
-      filter(Group %in% {RTsum %>%
-          filter(N > 1) %>%
-          .$Group})
-    
-    cors <- RTgroups %>%
-      split(.$Group) %>%
-      future_map(~{
-        analysisData(assignment@data,tibble(ID = 1:nrow(assignment@data))) %>%
-          keepFeatures(features = .x$Feature) %>%
-          {metabolyse(dat(.),sinfo(.),p,verbose = FALSE)} %>% 
-          analysisResults(element = 'correlations')
-      }) %>%
-      bind_rows(.id = 'RT Group')
-    
-  } else {
-    cors <- metabolyse(assignment@data,
-                       tibble(ID = 1:nrow(assignment@data)),
-                       p,
-                       verbose = FALSE) %>%
-      analysisResults(element = 'correlations') 
-  }
-  
-  assignment@correlations <- cors
+  assignment@correlations <- metabolyse(assignment@data,
+                     tibble(ID = 1:nrow(assignment@data)),
+                     p,
+                     verbose = FALSE) %>%
+    analysisResults(element = 'correlations') 
   
   if (assignment@log$verbose == T) {
     endTime <- proc.time()
