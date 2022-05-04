a2bc8de10295399c20541b6bbc614f1b9afae4fa
jasenfinch <jsf9@aber.ac.uk>
2022-05-04 14:13:36 +0100

    correct AIS and component plausibility calculation

--- R/components.R
+++ R/components.R
@@ -3,8 +3,8 @@ degree <- function(n_nodes,n_edges){
   2 * (n_edges / n_nodes)
 }
 
-plausibility <- function(AIS,degree,weight){
-  AIS + weight
+plausibility <- function(size,AIS,weight){
+  size * AIS + weight
 }
 
 #' @importFrom purrr compact
@@ -52,7 +52,7 @@ componentMetrics <- function(component,max_add_iso_total){
          Degree = degree(Nodes,Size),,
          Density = (2 * Size) / (Nodes * (Nodes - 1)),
          Weight = sum(Weight) / Nodes,
-         AIS = Size * sum(AIS) / max_add_iso_total,
+         AIS = sum(AIS) / max_add_iso_total,
          `Component Plausibility` = plausibility(AIS,Degree,Weight))
 }
 
